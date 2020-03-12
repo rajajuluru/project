2b16d503a0a883146904324c86395e3446a70762
lenovo <lenovo@DESKTOP-2D5RQTT>
2020-03-12 22:07:16 +0530

    first commting

---
+++ Project/src/main/java/com/project/entity/Project.java
@@ -0,0 +1,45 @@
+package com.project.entity;
+
+import java.sql.Date;
+
+@Entity
+public class Project {
+	
+	@Id
+	private Integer id;
+	private String pname;
+	private String description;
+	private Date startdate;
+	private Date enddate;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+	public Date getEnddate() {
+		return enddate;
+	}
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+
+}
